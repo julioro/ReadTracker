feb86d94f54d6deb69a4e385bf6cf42bfd92e8d7
julioro <julio.rodriguezo@pucp.pe>
2020-07-13 19:20:00 -0500

    Crear, registrar usuario - estructura.

---
+++ app/src/main/java/com/example/readtracker/activity/RecoverUserActivity.java
@@ -0,0 +1,16 @@
+package com.example.readtracker.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.readtracker.R;
+
+public class RecoverUserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recover_user);
+    }
+}
