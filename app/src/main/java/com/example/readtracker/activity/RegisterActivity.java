abf118df325f45da191997dadaf86f4c1a8ebed8
julioro <julio.rodriguezo@pucp.pe>
2020-07-13 23:33:39 -0500

    Activity registrar esqueleto.

--- app/src/main/java/com/example/readtracker/activity/RegisterActivity.java
+++ app/src/main/java/com/example/readtracker/activity/RegisterActivity.java
@@ -3,14 +3,21 @@ package com.example.readtracker.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.readtracker.R;
 
-public class CreateUserActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_create_user);
+        setContentView(R.layout.activity_register);
+    }
+
+    public void registerRegisterUser(View view) {
+    }
+
+    public void registerCancelRegister(View view) {
     }
 }
