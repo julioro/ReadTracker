009ecb12a4786c3cb290bf98bfcddb1af1b98a62
Julio Rodriguez Ordaya <j84168183@china.huawei.com>
2020-07-23 22:47:56 -0500

    Logica para listar readings

--- app/src/main/java/com/example/readtracker/activity/ListReadingsActivity.java
+++ app/src/main/java/com/example/readtracker/activity/ListReadingsActivity.java
@@ -25,10 +25,19 @@ import android.view.View;
 public class ListReadingsActivity extends AppCompatActivity {
 
     User currentUser;
-
+    int LAUNCH_ADD_READING_ACTIVITY = 1;
+    int LAUNCH_VIEW_READING_ACTIVITY = 2;
     // Recycler & Adapter
     RecyclerView rView;
     ListReadingsAdapter rReadingsAdapter;
+
+    FirebaseFirestore db;
+    FirebaseStorage firebaseStorage;
+    StorageReference storageReference;
+
+    // Lista de readings
+    ArrayList<Incidence> listReadings = new ArrayList<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,28 +45,60 @@ public class ListReadingsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_list_readings);
         Log.d("msgxd", "Iniciando ListReadingsActivity");
         Intent intent = getIntent(); // Get serializable intent data
-        currentUser = (User) intent.getSerializableExtra("currentUser");
-        fillInfo(currentUser);
+        currentUserWithReadings = (User) intent.getSerializableExtra("currentUserWithReadings");
+        listReadings = currentUserWithReadings.getListReadings();
+        fillInfo(listReadings);
 
     }
 
     /**
      * Fill recycler view.
      */
-    public void fillInfo(User user) {
-        rReadingsAdapter = new ListReadingsAdapter(user.getListReadings(), ListReadingsActivity.this);
+    public void fillInfo() {
+        rReadingsAdapter = new ListReadingsAdapter(listReadings, ListReadingsActivity.this);
         rView = findViewById(R.id.listReadingsRecyclerView);
         rView.setLayoutManager(new LinearLayoutManager(ListReadingsActivity.this));
         rView.setAdapter(rReadingsAdapter);
+
+        rReadingsAdapter.setOnItemClickListener(new ListReadingsAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(int position) {
+                Reading readingSelected = listReadings.get(position);
+                String readingTitleSelected = readingSelected.getTitle();
+                String urlSelected = readingSelected.getUrl();
+                String labelSelected = readingSelected.getLabel();
+                String authorSelected = readingSelected.getAuthor();
+                
+                String pagesSelected = String.valueOf(readingSelected.getPages());
+                boolean status = readingSelected.isStatus();
+                if (status){
+                    Datea dateSelected = readingSelected.getDate();
+                }
+
+                Intent intent = new Intent(ListIncidencesActivity.this, ViewIncidenceActivity.class);
+                intent.putExtra("readingSelected", readingSelected);
+                /*intent.putExtra("incidenceNameSelected", incidenceNameSelected);
+                intent.putExtra("incidenceDescriptionSelected", incidenceDescriptionSelected);
+                intent.putExtra("incidenceStatusSelected", incidenceStatusSelected);
+                intent.putExtra("incidenceCommentSelected", incidenceCommentSelected);
+                intent.putExtra("rol", rol);
+                intent.putExtra("incidenceOwnerSelected", incidenceOwnerSelected);
+                */
+                startActivityForResult(intent, LAUNCH_VIEW_READING_ACTIVITY);
+            }
+        });
+
     }
+
 
 
     /**
      * Button agregar una nueva lectura.
      */
-    public void buttonAddNewReading(View view) {
+    public void actionAddIncAppBar(MenuItem item) {
         Intent intent = new Intent(ListReadingsActivity.this, AddNewReadingActivity.class);
-        startActivity(intent);
+        intent.putExtra("userId", currentUserWithReadings.getUsuarioId);
+        startActivityForResult(intent, LAUNCH_ADD_READING_ACTIVITY);
     }
 
     /**
@@ -67,5 +108,50 @@ public class ListReadingsActivity extends AppCompatActivity {
         Intent intent = new Intent(ListReadingsActivity.this, DetailsReadingActivity.class);
         startActivity(intent);
     }
+
+
+
+    /**
+     * Listener para los cambios
+     */
+    public void readingsEventsListener(String userId) {
+        firebaseStorage = FirebaseStorage.getInstance();
+        storageReference = firebaseStorage.getReference();
+        db = FirebaseFirestore.getInstance();
+        String libraryPath = "readings/" + userId + "/library";
+        
+        databaseReference.child(libraryPath).addSnapshotListener(new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot snapshot, @Nullable FirebaseFirestoreException e) {
+                if (e != null) {
+                    Log.w(TAG, "Listen failed.", e);
+                    return;
+                }
+                if (dataSnapshot.getValue() != null) {
+                    Log.d("msgxd", dataSnapshot.getValue().toString());
+
+                    listReadings.clear();
+                    // Iterar
+                    for (DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
+                        Reading r = postSnapshot.getValue(Reading.class);
+                        listReadings.add(r);  // agregar todas las readings a un arreglo
+                        Log.d("msgxd", r.getTitle(); // imprimir desde un List
+                    }
+                    fillInfo();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) { // si hay un error al obtener la información en Firebase
+
+            }
+        });
+
+    }
+
+
+
+
+
 
 }
